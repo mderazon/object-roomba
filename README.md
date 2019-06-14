7c37aee35bc9de8c8546e7a3f60ae8f2cab04ffd
Michael DeRazon <mderazon@gmail.com>
2019-06-14 13:34:48 +0300

    update deps

--- README.md
+++ README.md
@@ -1,39 +1,39 @@
-# object-roomba [![Build Status](https://travis-ci.org/mderazon/object-roomba.svg?branch=master)](https://travis-ci.org/mderazon/object-roomba)
+# object-roomba [![Build Status](https://travis-ci.org/mderazon/object-roomba.svg?branch=master)](https://travis-ci.org/mderazon/object-roomba) [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 
 > Clean your dirty objects
 
-[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
-
-Userful when needing to parse objects from source that doesn't support data types declerations like csv files etc.
+Useful when needing to parse objects from source that doesn't support data types decelerations like csv files etc. Not intended for complex objects
 
 ## Usage
 
-``` js
-var roomba = require('object-roomba')
+```js
+const roomba = require('object-roomba');
 
-var schema = {
+const schema = {
   age: Number,
   expiration: Date,
   is_active: Boolean,
   name: String,
-  address: function (input) {
-    if (!input) { return null }
-    return input
+  address: input => {
+    if (!input) {
+      return null;
+    }
+    return input;
   }
-}
+};
 
-var clean = roomba(schema)
+const clean = roomba(schema);
 
-var dirty_obj = {
+let dirtyObj = {
   age: '18',
   expiration: '2015-08-17 19:30:30.219',
   is_active: 'false',
   name: 'Michael D',
   address: ''
-}
+};
 
-clean_obj = clean(dirty_obj)
-console.log(clean_obj)
+cleanObj = clean(dirtyObj);
+console.log(cleanObj);
 // {
 //   age: 18,
 //   expiration: Mon Aug 17 2015 19:30:30 GMT+0300 (IDT),
@@ -43,17 +43,21 @@ console.log(clean_obj)
 // }
 ```
 
-### Currently supporting
+### Currently only supporting simple types
+
 - String
 - Number
 - Date
 - Boolean
 
 ### Options
+
 You can pass options to roomba:
-``` js
-var clean = roomba(opts, schema)
+
+```js
+const clean = roomba(opts, schema);
 ```
+
 Available options are:
 
 - `remove_extra_fields` - By default, fields that are not in the schema are kept untouched, you can remove them from the cleaned obj by setting this to true.
--- test/test.js
+++ test/test.js
@@ -3,93 +3,91 @@
   global it: true
 */
 
-var roomba = require('..')
-var expect = require('chai').expect
+var roomba = require('..');
+var expect = require('chai').expect;
 
-function vaccum (type, val) {
-  var clean = roomba({ key: type })
-  return clean({key: val}).key
+function vaccum(type, val) {
+  var clean = roomba({ key: type });
+  return clean({ key: val }).key;
 }
 
-describe('sanitizers', function () {
-
+describe('sanitizers', function() {
   // boolean
-  it('should return a boolean', function () {
-    expect(vaccum(Boolean, true)).to.equal(true)
-    expect(vaccum(Boolean, false)).to.equal(false)
-    expect(vaccum(Boolean, '')).to.equal(false)
-    expect(vaccum(Boolean, NaN)).to.equal(false)
-    expect(vaccum(Boolean, null)).to.equal(false)
-    expect(vaccum(Boolean, undefined)).to.equal(false)
-    expect(vaccum(Boolean, 'true')).to.equal(true)
-    expect(vaccum(Boolean, 'false')).to.equal(false)
-    expect(vaccum(Boolean, '0')).to.equal(true)
-    expect(vaccum(Boolean, 0)).to.equal(false)
-    expect(vaccum(Boolean, {})).to.equal(true)
-  })
+  it('should return a boolean', function() {
+    expect(vaccum(Boolean, true)).to.equal(true);
+    expect(vaccum(Boolean, false)).to.equal(false);
+    expect(vaccum(Boolean, '')).to.equal(false);
+    expect(vaccum(Boolean, NaN)).to.equal(false);
+    expect(vaccum(Boolean, null)).to.equal(false);
+    expect(vaccum(Boolean, undefined)).to.equal(false);
+    expect(vaccum(Boolean, 'true')).to.equal(true);
+    expect(vaccum(Boolean, 'false')).to.equal(false);
+    expect(vaccum(Boolean, '0')).to.equal(true);
+    expect(vaccum(Boolean, 0)).to.equal(false);
+    expect(vaccum(Boolean, {})).to.equal(true);
+  });
 
   // string
-  it('should return a string', function () {
-    expect(vaccum(String, '0')).to.equal('0')
-    expect(vaccum(String, 0)).to.equal('0')
-    expect(vaccum(String, 0.1)).to.equal('0.1')
-    expect(vaccum(String, 'true')).to.equal('true')
-    expect(vaccum(String, 'false')).to.equal('false')
-    expect(vaccum(String, null)).to.be.null
-    expect(vaccum(String, undefined)).to.be.undefined
-  })
+  it('should return a string', function() {
+    expect(vaccum(String, '0')).to.equal('0');
+    expect(vaccum(String, 0)).to.equal('0');
+    expect(vaccum(String, 0.1)).to.equal('0.1');
+    expect(vaccum(String, 'true')).to.equal('true');
+    expect(vaccum(String, 'false')).to.equal('false');
+    expect(vaccum(String, null)).to.be.null;
+    expect(vaccum(String, undefined)).to.be.undefined;
+  });
 
   // number
-  it('should return a number', function () {
-    expect(vaccum(Number, 0)).to.equal(0)
-    expect(vaccum(Number, 1)).to.equal(1)
-    expect(vaccum(Number, 0.1)).to.equal(0.1)
-    expect(vaccum(Number, 1.0)).to.equal(1)
-    expect(vaccum(Number, -0.1)).to.equal(-0.1)
-    expect(vaccum(Number, -1)).to.equal(-1)
-    expect(vaccum(Number, -0)).to.equal(0)
-    expect(vaccum(Number, '0')).to.equal(0)
-    expect(vaccum(Number, '0.1')).to.equal(0.1)
-    expect(vaccum(Number, '1.0')).to.equal(1)
-    expect(vaccum(Number, '1')).to.equal(1)
-    expect(vaccum(Number, '-1')).to.equal(-1)
-    expect(vaccum(Number, '-0.1')).to.equal(-0.1)
-    expect(vaccum(Number, null)).to.equal(0)
-    expect(isNaN(vaccum(Number, NaN))).to.be.true
-    expect(isNaN(vaccum(Number, {}))).to.be.true
-    expect(isNaN(vaccum(Number, '1abc'))).to.be.true
-  })
+  it('should return a number', function() {
+    expect(vaccum(Number, 0)).to.equal(0);
+    expect(vaccum(Number, 1)).to.equal(1);
+    expect(vaccum(Number, 0.1)).to.equal(0.1);
+    expect(vaccum(Number, 1.0)).to.equal(1);
+    expect(vaccum(Number, -0.1)).to.equal(-0.1);
+    expect(vaccum(Number, -1)).to.equal(-1);
+    expect(vaccum(Number, -0)).to.equal(0);
+    expect(vaccum(Number, '0')).to.equal(0);
+    expect(vaccum(Number, '0.1')).to.equal(0.1);
+    expect(vaccum(Number, '1.0')).to.equal(1);
+    expect(vaccum(Number, '1')).to.equal(1);
+    expect(vaccum(Number, '-1')).to.equal(-1);
+    expect(vaccum(Number, '-0.1')).to.equal(-0.1);
+    expect(vaccum(Number, null)).to.equal(0);
+    expect(isNaN(vaccum(Number, NaN))).to.be.true;
+    expect(isNaN(vaccum(Number, {}))).to.be.true;
+    expect(isNaN(vaccum(Number, '1abc'))).to.be.true;
+  });
 
   // date
-  it('should return a date', function () {
-    expect(vaccum(Date, 'Wed Aug 19 2015 00:19:48 GMT+0300 (IDT)')).to.be.instanceOf(Date)
-    expect(vaccum(Date, 1439932788616)).to.be.null
-    expect(vaccum(Date, '1abc')).to.be.null
-    expect(vaccum(Date, null)).to.be.null
-    expect(vaccum(Date, NaN)).to.be.null
-  })
+  it('should return a date', function() {
+    expect(vaccum(Date, 'Wed Aug 19 2015 00:19:48 GMT+0300 (IDT)')).to.be.instanceOf(Date);
+    expect(vaccum(Date, 1439932788616)).to.be.null;
+    expect(vaccum(Date, '1abc')).to.be.null;
+    expect(vaccum(Date, null)).to.be.null;
+    expect(vaccum(Date, NaN)).to.be.null;
+  });
 
   // custom
-  it('should use custom function', function () {
-    var custom = function (input) {
-      return input * 2
-    }
-    expect(vaccum(custom, 1)).to.equal(2)
-  })
-})
+  it('should use custom function', function() {
+    var custom = function(input) {
+      return input * 2;
+    };
+    expect(vaccum(custom, 1)).to.equal(2);
+  });
+});
 
-describe('extra fields', function () {
-  var schema = { a: String }
-  var obj = { a: 1, b: 2 }
+describe('extra fields', function() {
+  var schema = { a: String };
+  var obj = { a: 1, b: 2 };
 
-  it('should keep extra fields', function () {
-    var clean = roomba(schema)
-    expect(clean(obj)).to.have.property('b')
-  })
+  it('should keep extra fields', function() {
+    var clean = roomba(schema);
+    expect(clean(obj)).to.have.property('b');
+  });
 
-  it('should remove extra fields when option is set', function () {
-    var clean = roomba({ remove_extra_fields: true }, schema)
-    expect(clean(obj)).to.not.have.property('b')
-  })
-
-})
+  it('should remove extra fields when option is set', function() {
+    var clean = roomba({ remove_extra_fields: true }, schema);
+    expect(clean(obj)).to.not.have.property('b');
+  });
+});
